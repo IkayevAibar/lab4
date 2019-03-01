2a7ff4e4d238d1160a03a142a2ecbc2e4e81f77c
IkayevAibar <160107095@stu.sdu.edu.kz>
2019-03-01 12:14:00 +0300

    8

--- resources/views/layouts/friends/friends.blade.php
+++ resources/views/layouts/friends/friends.blade.php
@@ -6,7 +6,7 @@
         </br>
         @foreach($friends as $friend )
             {{$friend->name}}
-            <a type="button" name="" id="" class="btn btn-primary" btn-lg btn-block">send message</a>
+            <a type="button" action="friend/{{$friend->id}}/sendMessage" name="" id="" class="btn btn-primary" btn-lg btn-block">send message</a>
         @endforeach
     </div>
     
